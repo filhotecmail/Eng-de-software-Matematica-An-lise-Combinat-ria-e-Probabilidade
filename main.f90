1a98f0be8dd81515b1efc48b71069f5aaac0a900
Carlos Alberto Dias da S. Filho <filhotecmail@gmail.com>
2026-02-18 21:23:56 -0300

    Entendimento Matematico

--- main.f90
+++ main.f90
@@ -1,4 +1,4 @@
-! ================================================================
+! ===============================================================================
 ! Autor: Carlos Alberto Dias da S. Filho
 ! Email: filhotecmail@gmail.com
 ! Data de Criação: 2025-02-01
@@ -7,21 +7,21 @@
 !   resolver o enunciado do Advent of Code 2025, dia 2.
 ! Objetivos: Resolver o problema do Dia 2 usando Fortran F90.
 !
-! Contexto matemático (lógica de entendimento do problema):
+! Contexto matemático:
 ! - Um ID inválido é um número decimal cuja representação é a
 !   concatenação de uma sequência de dígitos com ela mesma: S||S.
-! - Portanto, IDs inválidos têm número de dígitos par: 2k.
-! - Seja k = metade do comprimento e X o inteiro formado pelos
-!   primeiros k dígitos (sem zeros à esquerda). Então:
+! - IDs inválidos têm número de dígitos par: 2k.
+! - k = metade do comprimento e X o inteiro formado pelos
+!   primeiros k dígitos - sem zeros à esquerda . Então:
 !     N = X * 10^k + X = X * (10^k + 1).
 ! - Para um intervalo [L, R], e cada k em {1,2,3,4,5} (até 10 dígitos):
 !     B_k = 10^k + 1,
 !     X ∈ [ceil(L / B_k), floor(R / B_k)] ∩ [10^(k−1), 10^k − 1].
 !   Os candidatos inválidos são N = X * B_k.
-! - Essa forma evita varrer número a número: gera apenas N possíveis
-!   diretamente via X e k, garantindo eficiência e correção.
-! - Em caso de intervalos sobrepostos, N deve ser deduplicado antes da soma.
-! ================================================================
+! - Essa forma evita varrer número a número, gera apenas N possíveis
+!   diretamente via X e k.
+! - Para intervalos sobrepostos, N deve ser deduplicado antes da soma.
+! =================================================================================
 
 program aoc_day2
   implicit none
